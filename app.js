793d7fd56aa0d536dff57e0a9ba31e56a3f66939
Rene Correa <ratanky@hotmail.com>
2020-11-25 23:08:33 -0300

    console log from forms

--- app.js
+++ app.js
@@ -1,7 +1,30 @@
 const express = require("express");
-
 const app = express();
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+// mongoose.connect("mongodb+srv://user1:senha123@webdesign.fgics.mongodb.net/nodedb?retryWrites=true&w=majority", {
+//   useNewUrlParser: true
+// }, function(error){
+//   if(error){
+//     console.log(error);
+//   } else {
+//     console.log("Connected to database");
+//   }
+// });
+
+// var profileSchema = new mongoose.Schema({
+//   name: String,
+//   email: String,
+//   password: String,
+//   Age: Number,
+//   dateRegistration: Date
+//   role: String
+// });
+// var Profile = mongoose.model("Profile", profileSchema);
+
+var login = "email@email.com";
+var senha = "123456";
 
 app.use(express.static("public"));
 
@@ -13,16 +36,27 @@ app.get("/", function (req, res) {
   res.render("login");
 });
 
+app.post("/login",function(req,res){
+  var data = req.body;
+  console.log(data);
+  if(data.email == login && data.password == senha){
+    res.redirect("/status");
+  } else {
+    console.log("Login error.");
+    res.redirect("/");
+  }
+});
+
+app.get("/status", function (req, res) {
+  res.render("status");
+});
+
 app.get("/profile", function (req, res) {
   res.render("profile");
 });
 
 app.get("/requirement", function (req, res) {
   res.render("requirement");
-});
-
-app.get("/status", function (req, res) {
-  res.render("status");
 });
 
 app.get("/create", function (req, res) {
@@ -32,11 +66,19 @@ app.get("/create", function (req, res) {
 app.post("/create", function (req, res) {
   var data = req.body;
   console.log(data);
-  res.redirect("/");
+  if(data.password1 != data.password2){
+    console.log("Different passwords");
+    res.redirect("create");
+  } else {
+    res.redirect("/");
+  }
+  
 });
 
 app.get("/forgot", function (req, res) {
-  res.render("forgotpassword");
+  res.render("forgotpassword");app.get("/status", function (req, res) {
+  res.render("status");
+}); 
 });
 
 app.post("/forgot", function (req, res) {
